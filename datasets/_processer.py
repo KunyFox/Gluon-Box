fa4d15fd6fcbf501bbd911a710a405a7dc7e8a17
KyuanCortex <yuankun@cortexlabs.ai>
2020-12-08 19:00:44 +0800

    update processer

--- datasets/_processer.py
+++ datasets/_processer.py
@@ -3,8 +3,8 @@ from utils import ModuleRegister
 PROCESSER = ModuleRegister('backbone')
 
 def get_processer(processer):
-    assert isinstance(cfg, list), 
-        "processer must be a list, but got {}".format(type(cfg))
+    assert isinstance(cfg, dict), 
+        "processer must be a dict, but got {}".format(type(cfg))
 
     assert processer[-1] == 'ToBatch', "Processer should end of 'ToBatch'."
 
@@ -14,6 +14,6 @@ def get_processer(processer):
         f = PROCESSER.get(p, None)
         if f is None:
             raise ValueError("{} is not registed in processer!".format(f))
-        processer_f.append(f)
+        processer_f.append(f(**processer[p]))
     
     return processer_f
